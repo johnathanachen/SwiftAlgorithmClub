4c303f0b4a6a559b8de03ffce202683ecc8c1129
Johnathan Chen <johnathanchenn@outlook.com>
2017-11-29 15:42:41 -0800

    iterative binary search

--- BinarySearch.playground/Contents.swift
+++ BinarySearch.playground/Contents.swift
@@ -30,6 +30,16 @@ func binarySearchIterative<T: Comparable>(_ a: [T], key: T) -> Int? {
     var lowerBound = 0
     var upperBound = a.count
     while lowerBound < upperBound {
-        
+        let midIndex = lowerBound + (upperBound - lowerBound) / 2
+        if a[midIndex] == key {
+            return midIndex
+        } else if a[midIndex] < key {
+            lowerBound = midIndex + 1
+        } else {
+            upperBound = midIndex
+        }
     }
+    return nil
 }
+
+binarySearchIterative(numbers, key: 19)
